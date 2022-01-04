b3ff70c7ac8be94e862544bb000eef4690d1dcf9
Epiphron Quant <91112822+epiphronquant@users.noreply.github.com>
2022-01-04 13:04:41 +0800

    Update HKEX_Short_Positions_App.py

--- HKEX_Short_Positions_App.py
+++ HKEX_Short_Positions_App.py
@@ -14,7 +14,7 @@ import datetime as dt
 st.set_page_config(layout="wide")
 st.title('HKEX Short Positions')
 
-@st.cache(ttl= 1800)
+@st.cache(ttl= 1800) ## Data would need to get reloaded every 30 minutes
 def load_data(link):
     df = pd.read_excel(link,sheet_name='Sheet1', header = 0, engine = 'openpyxl', parse_dates = False)
     df = df.loc[df['Listed?'] == 1] ### Filters rows where it is still listed
@@ -36,7 +36,7 @@ slider = tuple([100000000*x for x in slider])
 df = df[df['Market Cap (Dec 21)'].between(slider [0], slider [1])]
 
 column_1, column_2 = st.columns(2) ### Divides page into 2 columns
-with column_1:## dropdown for share measurement
+with column_1:## dropdown box for share measurement
     share_measurement = ["Share Shorted %", "Aggregated Reportable Short Positions (HK$)"]
     share_measurement = st.selectbox(
         'Which measure of shares shorted should we use?',
@@ -56,6 +56,7 @@ sector = st.selectbox(
       sectors)
 'You selected: ', sector
  
+### create barchart given data
 def bar_chart(x, y1, y2, title, tickformat = ''):
     fig = go.Figure(
             data=[go.Bar(name='Count', x=x, y=y1, yaxis='y', offsetgroup=1),
@@ -73,29 +74,27 @@ with column_1:### Sector Chart
         chart1 = df [['Aggregated Reportable Short Positions (HK$)','Share Shorted %','Sector', 'Stock Name', 'Date']]
         date = chart1 ['Date'].max()
         chart1 = chart1.loc[df['Date'] == date]
-        a = chart1.groupby(['Stock Name','Sector']).count() ### gathers data by Lead 1
+        a = chart1.groupby(['Stock Name','Sector']).count() ### gathers data by sector
         a = a.groupby (['Sector']).count()
         industries = a.index
         industries = industries.tolist()
         
-        a = a[share_measurement] ### data column that shows deal count
+        a = a[share_measurement]
         a = a.rename('Count')
         a  = a.to_list()
         if select_central == 'Average':
-            b = chart1.groupby(['Sector']).mean() ### gathers data by Lead 1
-            b = b[share_measurement]
-        
+            b = chart1.groupby(['Sector']).mean() ### gathers data by sector
+            b = b[share_measurement]        
         else:
-            b = chart1.groupby(['Sector']).median() ### gathers data by Lead 1
-            b = b[share_measurement]
-        
+            b = chart1.groupby(['Sector']).median() ### gathers data by sector
+            b = b[share_measurement]        
         b = b.to_list()
         date = date.strftime('%b %d, %Y')
         fig = bar_chart(industries, a, b,"Sector Companies Count and " + select_central +' ' + share_measurement + ' as of ' + date)
         return fig  
     fig = chart_1(df, share_measurement, select_central)    
     st.plotly_chart(fig)
-
+### filters data by sector
 if sector == 'All':
     df = df 
 else:
@@ -105,28 +104,26 @@ with column_2:### Industry Chart
     chart1 = df [['Aggregated Reportable Short Positions (HK$)','Share Shorted %', 'Stock Name', 'Industry', 'Date']]
     date = chart1 ['Date'].max()
     chart1 = chart1.loc[df['Date'] == date]
-    a = chart1.groupby(['Stock Name','Industry']).count() ### gathers data by Lead 1
+    a = chart1.groupby(['Stock Name','Industry']).count() ### gathers data by industry
     a = a.groupby (['Industry']).count()
     industries = a.index
     industries = industries.tolist()
-    a = a[share_measurement] ### data column that shows deal count
+    a = a[share_measurement] 
     a = a.rename('Count')
-    a  = a.to_list()
+    a  = a.to_list()    
     if select_central == 'Average':
-        b = chart1.groupby(['Industry']).mean() ### gathers data by Lead 1
-        b = b[share_measurement]
-    
+        b = chart1.groupby(['Industry']).mean() ### gathers data by industry
+        b = b[share_measurement]    
     else:
-        b = chart1.groupby(['Industry']).median() ### gathers data by Lead 1
+        b = chart1.groupby(['Industry']).median() ### gathers data by industry
         b = b[share_measurement]
     
     b = b.to_list()
     date = date.strftime('%b %d, %Y')
 
     fig = bar_chart(industries, a, b,sector +' Sub Sector' +" Companies Count and " + select_central +' ' + share_measurement + ' as of ' + date)
-
     st.plotly_chart(fig)
-    ### Sector performance with short measurement chart
+### Sector performance with short measurement chart
 performance = df [['Date', share_measurement]]
 if select_central == 'Average':
     a = performance.groupby(['Date']).mean()
@@ -135,14 +132,12 @@ else:
     a = performance.groupby(['Date']).median()
 
 fig = make_subplots(specs=[[{"secondary_y": True}]])
-
     #add a box to select HSI or HSH as second axis
 index = st.selectbox(
     'Which index would you like to compare it to?',
       ['Hang Seng Healthcare Index', 'Hang Seng Index'])
 'You selected: ', index
-
-    #Download HSH and HSI data
+    # Download HSH and HSI data
 start = a.index[0].strftime('%d/%m/%Y')
 end = a.index[-1].strftime('%d/%m/%Y')
 if index == 'Hang Seng Index':
@@ -200,7 +195,6 @@ else:
                                             from_date= start,
                                             to_date= end)
 fig = make_subplots(specs=[[{"secondary_y": True}]])
-
     # Add traces
 fig.add_trace(go.Scatter(x= a.index, y= a[share_measurement], name= select_central + ' '+ share_measurement),
     secondary_y=False)
@@ -216,15 +210,11 @@ fig.update_yaxes(title_text= industry + ' '+ select_central + ' '+ share_measure
 fig.update_yaxes(title_text="Index Level", secondary_y=True)
 st.plotly_chart(fig, use_container_width=True)
 
-########## chart displaying key information of raw data
-
 ## add a column displaying stock with the highest monthly increase in shortsell
-
 date = df ['Date'].max()
 df1 = df.loc[df['Date'] == date]
 d = dt.timedelta(days=28) ## selects one month. This must be done in multiples of 7 given the SFC dataset
 date2 = date - d
-
 df2 = df.loc[df['Date'] == date2]
 df_2 = df1 [['Stock Code', 'Share Shorted %']]
 df2 = df2.rename(columns = {'Share Shorted %':'Share Shorted % -1 month'})
@@ -237,9 +227,8 @@ df2 = df2 [['Last Month Share Short Pct Pt Change','Stock Code']]
 
 df1 = df1.merge(df2, on='Stock Code', how='left')
 
-
 date = date.strftime('%b %d, %Y')
-
+### chart displaying key information of raw data
 @st.cache()
 def chart_5(df1, select_central, sector):
     if select_central == 'Average':
@@ -266,7 +255,6 @@ df1
 
 ########## line chart on company
 performance = df [['Date', 'Stock Name', 'Yf Ticker', 'Stock Code', share_measurement]]
-
 companies = performance['Stock Name']
 companies = companies.tolist()
 companies = list(dict.fromkeys(companies))
@@ -274,17 +262,16 @@ tickers = performance['Stock Code']
 tickers = tickers.tolist()
 tickers = list(dict.fromkeys(tickers))
 companies.extend(tickers)
-company = st.selectbox(
+company = st.selectbox( ### can select tickers and company name for individual company chart
     'Which company/stock code are you interested in?',
       companies)
 'You selected: ', company
 
-if type(company) == int:
+if type(company) == int: ## if input is a ticker
     company = performance.loc[performance['Stock Code'] == company, 'Stock Name'] 
     company = company.iloc[0]
 else:
     pass    
-
 @st.cache(suppress_st_warning=False, allow_output_mutation=True)
 def chart_6 (performance, company, share_measurement, sector):
     performance = performance.loc[performance['Stock Name'] == company]
